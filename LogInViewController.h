029521600ea7844d927813a735cb6bc69cd5d524
Daniel Ramirez Calderon <Danger@Daniels-MacBook-Pro.local>
2014-11-25 20:26:23 -0400

    Added Parse and view controller changes

---
+++ LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  Inqui
+//
+//  Created by Daniel Ramirez Calderon on 11/25/14.
+//  Copyright (c) 2014 Inqui Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
---
+++ LogInViewController.m
@@ -0,0 +1,41 @@
+//
+//  LogInViewController.m
+//  Inqui
+//
+//  Created by Daniel Ramirez Calderon on 11/25/14.
+//  Copyright (c) 2014 Inqui Project. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (IBAction)usernameText:(UITextField *)sender {
+    NSLog(@"Nigga We made it!");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
