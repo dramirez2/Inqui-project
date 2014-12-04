47c297004b5aff9ec1cb37235500c408f919bce8
dramirez2 <bojangles7856@gmail.com>
2014-12-04 00:31:48 -0400

    Git ignore
    	modified:   AppDelegate.m
    	modified:   DefaultSettingsViewController.h
    	modified:   LoginUserViewController.h
    	modified:   LoginUserViewController.m
    	modified:   ResearchViewViewController.h
    	modified:   ResearchViewViewController.m
    	modified:   SignInViewController.h
    	modified:   SignInViewController.m
    	modified:   StartViewController.h
    	modified:   StartViewController.m
    	modified:   researchDoc.h
    	modified:   researchDoc.m
    	modified:   testViewController.xib

---
+++ DefaultSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignOrLogViewController.h
+//  Inqui
+//
+//  Created by Daniel Ramirez Calderon on 12/3/14.
+//  Copyright (c) 2014 Inqui Project. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SignOrLogViewController : ViewController
+
+@end
---
+++ StartViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartViewController.h
+//  Inqui
+//
+//  Created by Daniel Ramirez Calderon on 12/3/14.
+//  Copyright (c) 2014 Inqui Project. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface StartViewController : ViewController
+
+@end
