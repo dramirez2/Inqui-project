d6c720d7662ff5827eb72c94e4b2f9b65ce8f5f1
dramirez2 <bojangles7856@gmail.com>
2014-12-14 13:13:47 -0400

    Added instructions on scrollview.

--- researchDocViewController.m
+++ researchDocViewController.m
@@ -19,7 +19,7 @@
     NSLog(@"Entered here");
     self.title = @"Details";
     self.view.backgroundColor = [UIColor whiteColor ];
-    
+    //IVAN, ESTE LABEL ES LO QUE APARECE EN EL SCROLL VIEW, SE PONE EN EL VIEW
     UILabel *alabel = [[UILabel alloc] initWithFrame:(CGRectMake(0, 0, self.view.bounds.size.width, 100))];
     
     alabel.textColor = [UIColor blackColor];
@@ -32,17 +32,19 @@
 //        self.title = [NSString stringWithFormat:@"%@",object[@"Title"]];
         NSLog(@"The likes of this thing is %@",object[@"Likes"]);
     }];
-    
+    //IVAN, AQUI ESTA EL VIEW DONDE LE A~ADES EL LABEL, Y ESTE VIEW LO A~ADES AL SCROLLVIEW
     UIView *testing = [[UIView alloc] initWithFrame:(CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height))];
+    //EL ACTO DE PONER EL LABEL EN EL VIEW
     [testing addSubview:alabel];
     testing.backgroundColor = [UIColor whiteColor];
     
+    //ESTE ES EL SCROLLVIEW COMO TAL
     UIScrollView *scrolling = [[UIScrollView alloc] initWithFrame:(CGRectMake(0, 100, self.view.bounds.size.width, self.view.bounds.size.height))];
     scrolling.scrollEnabled = YES;
     scrolling.showsVerticalScrollIndicator = YES;
     
 //    scrolling.backgroundColor = [UIColor redColor];
-    
+    //EL ACTO DE PONER EL VIEW EN EL SCROLLVIEW
     [scrolling addSubview:testing];
     scrolling.contentSize = testing.frame.size;
     
